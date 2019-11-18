937cee517288efd4125db0432c8f98fccf12e8ab
Virgilio Santos <virgilio.santos@siriusxm.com>
2019-11-18 18:48:10 -0300

    frontend: lean up App component to only Router specific

--- cs_frontend/src/App.js
+++ cs_frontend/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 
 import VideoEditView from './views/Video/VideoEditView';
 import VideoListView from './views/Video/VideoListView';
 import VideoView from './views/Video/VideoView';
 import SignIn from './views/Auth/SignIn'
 import SignUp from './views/Auth/SignUp'
-import { AppBar, Typography, Toolbar } from '@material-ui/core';
 
 import {
   BrowserRouter as Router,
@@ -14,27 +12,10 @@ import {
   Route,
 } from "react-router-dom";
 
-const useStyles = makeStyles(theme => ({
-  appBar: {
-    position: 'relative',
-  },
-}));
-
-function App() {
-  const classes = useStyles();
-
+const App = () => {
   return (
     <Router>
       <div>
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <AppBar position="absolute" color="default" className={classes.appBar}>
-          <Toolbar>
-            <Typography variant="h6" color="inherit" noWrap>
-              Company name
-            </Typography>
-          </Toolbar>
-        </AppBar>
         <Switch>
           <Route path="/login">
             <SignIn />
@@ -45,9 +26,7 @@ function App() {
           <Route path="/video/edit">
             <VideoEditView />
           </Route>
-          <Route path="/video">
-            <VideoView />
-          </Route>
+          <Route path="/video/:video" component={VideoView} />
           <Route path="/">
             <VideoListView />
           </Route>
